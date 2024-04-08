c65ca750fcfb9cf8b3c828f9a9cb59e335e8f963
JerryKing <106170474+ReL001@users.noreply.github.com>
2024-04-08 14:04:04 +0530

    Setting up Controller and Service class. Printing the all movies data from the database.

--- src/main/java/com/example/Movies/Api/MovieService.java
+++ src/main/java/com/example/Movies/Api/MovieService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Service
 public class MovieService {
-    @Autowired
+    @Autowired  //he @Autowired annotation is used in Spring to automatically inject dependencies into your beans. This can be done on fields, constructors, and methods. When you use @Autowired, Spring looks for a bean that matches the property it is annotating, and injects it automatically.  In your code, you have used @Autowired in your MovieService and MovieController classes. In MovieService, it is used to inject an instance of MovieRepository. In MovieController, it is used to inject an instance of MovieService.
     private MovieRepository MovieRepo;
     public List<Movie> allMovies(){
         return MovieRepo.findAll();
