8d297b34227daeeb6e06ec8c570ec7e7c8e7fc86
JerryKing <106170474+ReL001@users.noreply.github.com>
2024-04-09 15:13:00 +0530

    BAckend completed

--- src/main/java/com/example/Movies/Api/ReviewRepository.java
+++ src/main/java/com/example/Movies/Api/ReviewRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends MongoRepository {
+public interface ReviewRepository extends MongoRepository<Review, String> {
+
 }
